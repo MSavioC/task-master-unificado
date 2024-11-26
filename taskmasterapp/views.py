9b01673232fd6d60c03b75c057158986d9c65de4
MSavioC <dogehkinn@gmail.com>
2024-11-26 16:19:18 -0300

    first commit

---
+++ taskmasterapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def login_view(request):
+    return render(request, "login/index.html")
+def tasklist_view(request):
+    return render(request, 'tasklist/index.html')
